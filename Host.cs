a79a6dc9aa224d9474a8e5bbc38422bba2043ac2
Koen J <junk@koenj.com>
2015-09-25 20:42:46 +0200

    Added function for resolving IP to hostname, useful for viewing cached pages.

--- Host.cs
+++ Host.cs
@@ -26,6 +26,20 @@ namespace PassiveScanning
             Address = address;
             AddressString = Address.ToString();
         }
+
+        public string GetHostname()
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                string responseString = client.UploadString("https://www.whatismyip.com/custom/response.php", "hostname" + AddressString + "=&action=hostname-lookup");
+                int start = responseString.IndexOf('>') + 1;
+                int end = responseString.IndexOf('<', start);
+
+                string hostName = responseString.Substring(start, end - start);
+                return hostName;
+            }
+        }
     }
 }
 
